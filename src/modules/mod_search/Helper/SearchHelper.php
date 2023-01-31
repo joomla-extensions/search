947f0b54bdef7eb666aad4cb9d8c5dade42b48ea
George Wilson <georgejameswilson@googlemail.com>
2023-01-31 21:31:27 +0000

    Use robo to update headers

--- src/modules/mod_search/Helper/SearchHelper.php
+++ src/modules/mod_search/Helper/SearchHelper.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  mod_search
+ * @package     Joomla.Administrator
+ * @subpackage  Search
  *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2005 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
